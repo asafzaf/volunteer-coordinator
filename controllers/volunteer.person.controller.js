6a0a482aecd607ecd9633e9b71706497f7648032
asafzaf <asaf.zafrir@gmail.com>
2024-03-04 13:48:47 +0200

    added another object

---
+++ controllers/volunteer.person.controller.js
@@ -0,0 +1,37 @@
+const e = require("express");
+const catchAsync = require("../utils/catchAsync");
+
+exports.getAllVolunteerPersons = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "This route is not yet defined!",
+  });
+});
+
+exports.getVolunteerPersonById = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "This route is not yet defined!",
+  });
+});
+
+exports.createVolunteerPerson = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "This route is not yet defined!",
+  });
+});
+
+exports.updateVolunteerPerson = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "This route is not yet defined!",
+  });
+});
+
+exports.deleteVolunteerPerson = catchAsync(async (req, res, next) => {
+  res.status(200).json({
+    status: "success",
+    message: "This route is not yet defined!",
+  });
+});
---
+++ routers/volunteer.person.router.js
@@ -0,0 +1,17 @@
+const express = require("express");
+
+const {
+    getAllVolunteerPersons,
+    getVolunteerPersonById,
+    createVolunteerPerson,
+    updateVolunteerPerson,
+    deleteVolunteerPerson,
+    } = require("../controllers/volunteer.person.controller");
+
+const router = express.Router();
+
+router.route("/").get(getAllVolunteerPersons).post(createVolunteerPerson);
+
+router.route("/:id").get(getVolunteerPersonById).patch(updateVolunteerPerson).delete(deleteVolunteerPerson);
+
+module.exports = router;
