4ecdfb2b29b58682b47f116b3ac9dba0b4665683
asafzaf <asaf.zafrir@gmail.com>
2024-03-18 21:07:44 +0200

    lint

--- routers/volunteer.person.router.js
+++ routers/volunteer.person.router.js
@@ -1,17 +1,21 @@
 const express = require("express");
 
 const {
-    getAllVolunteerPersons,
-    getVolunteerPersonById,
-    createVolunteerPerson,
-    updateVolunteerPerson,
-    deleteVolunteerPerson,
-    } = require("../controllers/volunteer.person.controller");
+  getAllVolunteerPersons,
+  getVolunteerPersonById,
+  createVolunteerPerson,
+  updateVolunteerPerson,
+  deleteVolunteerPerson,
+} = require("../controllers/volunteer.person.controller");
 
 const router = express.Router();
 
 router.route("/").get(getAllVolunteerPersons).post(createVolunteerPerson);
 
-router.route("/:id").get(getVolunteerPersonById).patch(updateVolunteerPerson).delete(deleteVolunteerPerson);
+router
+  .route("/:id")
+  .get(getVolunteerPersonById)
+  .patch(updateVolunteerPerson)
+  .delete(deleteVolunteerPerson);
 
 module.exports = router;
