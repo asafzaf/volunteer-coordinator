5ba09dd1b4119279590bf5256926f2bd7848be57
Eliya <eliysamaea@gmail.com>
2024-04-18 13:22:10 +0300

    adding an update for the volunteers array (to add the user with sign)

--- controllers/volunteer.task.controller.js
+++ controllers/volunteer.task.controller.js
@@ -31,26 +31,31 @@ exports.updateVolunteerTask = catchAsync(async (req, res, next) => {
   res.status(200).send(updateVolunteerTask);
 });
 
-exports.updateVolunteers = catchAsync(async (req, res, next) => {
-  // update the volunteers array
-  const { id } = req.params;
-  const { volunteers } = req.body;
+exports.updateVolunteers = catchAsync(
+  async (taskId, userId, req, res, next) => {
+    // update the volunteers array
+    try {
+      const volunteering = await volunteerTaskRepository.retrieve(taskId);
+      if (!volunteering) {
+        return res.status(404).json({ message: "Volunteering task not found" });
+      }
 
-  try {
-    const volunteering = await volunteerTaskRepository.retrieve(id);
-    if (!volunteering) {
-      return res.status(404).json({ message: "Volunteering task not found" });
+      const updatedTask = {
+        ...volunteering,
+        volunteers: [...volunteering.volunteers, userId],
+      };
+
+      const updatedVolunteering = await volunteerTaskRepository.put(
+        taskId,
+        updatedTask
+      );
+
+      res.status(200).json(updatedVolunteering);
+    } catch (error) {
+      res.status(200).send(updatedVolunteering);
     }
-
-    volunteering.volunteers = volunteers;
-
-    const updatedVolunteering = await volunteering.put(id, volunteering);
-
-    res.status(200).json(updatedVolunteering);
-  } catch (error) {
-    res.status(200).send(updateVolunteerTask);
   }
-});
+);
 
 exports.deleteVolunteerTask = catchAsync(async (req, res, next) => {
   const deleteVolunteerTask = await volunteerTaskRepository.delete(
