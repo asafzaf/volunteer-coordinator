e8f6dd2b210473c060bbdd40055382971e2583e1
Eliya <eliysamaea@gmail.com>
2024-04-18 13:28:40 +0300

    adding a route for updateVolunteers

--- routers/volunteer.task.router.js
+++ routers/volunteer.task.router.js
@@ -5,12 +5,19 @@ const {
   createVolunteerTask,
   updateVolunteerTask,
   deleteVolunteerTask,
+  updateVolunteers,
 } = require("../controllers/volunteer.task.controller");
 
 const router = express.Router();
 
 router.route("/").get(getAllVolunteerTasks).post(createVolunteerTask);
 
-router.route("/:id").get(getVolunteerTaskById).patch(updateVolunteerTask).delete(deleteVolunteerTask);
+router
+  .route("/:id")
+  .get(getVolunteerTaskById)
+  .patch(updateVolunteerTask)
+  .delete(deleteVolunteerTask);
+
+router.route("/:userid/:taskid").patch(updateVolunteers);
 
 module.exports = router;
