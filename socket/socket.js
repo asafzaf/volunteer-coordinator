1d1384f292f4807d1547457b7566b53b1711fbf4
asafzaf <asaf.zafrir@gmail.com>
2024-04-23 23:07:44 +0300

    added terms

--- socket/socket.js
+++ socket/socket.js
@@ -6,7 +6,13 @@ function handleSocket(server) {
   io.on("connection", async (socket) => {
     const userId = socket.handshake.query.user_id;
     let userId1 = userId.replace(/['"]+/g, "");
+    if (!userId1) {
+      return;
+    }
     const user = await userRepository.retrieve(userId1);
+    if (!user) {
+      return;
+    }
     let userTasks = user.tasks;
     userTasks.forEach((taskId) => {
       socket.join(taskId);
